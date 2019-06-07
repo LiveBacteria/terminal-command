96d98683bf5cc2c2f5f9d3211500060f96314167
Poore <poore.tyler@cleanharbors.com>
2019-06-07 12:42:29 -0700

    Submission Commit

---
+++ terminal-commands.js
@@ -0,0 +1,37 @@
+const fs = require('fs');
+
+module.exports.ls = () => {
+    fs.readdir('./', (err, files) => {
+        const filesToString = files.reduce((acc, file) => {
+            return acc + file;
+        }, '');
+        console.log(filesToString);
+    });
+};
+
+module.exports.touch = () => {
+
+};
+
+module.exports.mkdir = () => {
+
+};
+
+/*
+Questions
+How do you import a module?
+You can import a module by using `require();`
+
+What JavaScript statement do you use to export a module?
+`module.exports`
+
+Online Research Questions
+Are there other ways to export a module?
+Yes, like the way we used in the react checkpoints and modules, you could use `export as` an ES6 feature.
+
+What are ES6 modules and how do they differ from the module we created in this checkpoint?
+
+There are a number of reasons the two are different. ES6 modules, aside from the syntax, are better suited for frontend coding. Where as CommonJS is more commonly used in server side code.
+
+
+ */
